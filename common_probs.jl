0b285fa33f6b8f49cbddd7db70fffc74d11b43cc
alexanderscottcharles <alexanderscottcharles@gmail.com>
2024-07-21 19:43:55 -0700

    Add files via upload

---
+++ common_probs.jl
@@ -0,0 +1,41 @@
+module common_probs
+
+export common_prob_dict
+
+common_prob_dict = Dict(
+    "Coins" => 1//29,
+    "Air Runes" => 1//29,
+    "Fire Runes" => 1//29,
+    "Water Runes" => 1//29,
+    "Earth Runes" => 1//29,
+    "Law Runes" => 1//29,
+    "Nature Runes" => 1//29,
+    "Oak planks" => 1//29,
+    "Water Talisman" => 1//29,
+    "Air Talisman" => 1//29,
+    "Earth Talisman" => 1//29,
+    "Fire Talisman" => 1//29,
+    "Pure Essence" => 1//29,
+    "Staff of air" => 1//29,
+    "Staff of water" => 1//29,
+    "Staff of earth" => 1//29,
+    "Staff of fire" => 1//29,
+    "Hard leather" => 1//29,
+    "Huge plated mithril salvage" => 1//29,
+    "Medium plated mithril salvage" => 2//29,
+    "Small plated mithril salvage" => 1//29,
+    "Small bladed mithril salvage" => 1//29,
+    "Medium bladed mithril salvage" => 1//29,
+    "Mithril arrow" => 1//29,
+    "Amulet of magic" => 1//29,
+    "Amulet of magic (t)" => 1//290,
+    "Willow composite bow" => 1//87,
+    "Yew composite bow" => 1//87,
+    "Magic composite bow" => 1//87,
+    "Black crossbow" => 1//87,
+    "Offhand black crossbow" => 1//87,
+    "Black 2h crossbow" => 1//87,
+    "Black bolts" => 1//29, 
+)
+
+end
---
+++ common_values.jl
@@ -0,0 +1,40 @@
+module common_values
+
+export common_value_dict
+
+const common_value_dict = Dict(
+    "Coins" => 5000,
+    "Air Runes" => 8100,
+    "Fire Runes" => 12750,
+    "Water Runes" => 12300,
+    "Earth Runes" => 2850,
+    "Law Runes" => 4020,
+    "Nature Runes" => 14620,
+    "Oak planks" => 17200,
+    "Water Talisman" => 1293,
+    "Air Talisman" => 947,
+    "Earth Talisman" => 182,
+    "Fire Talisman" => 229,
+    "Pure Essence" => 23550,
+    "Staff of air" => 2218,
+    "Staff of water" => 2551,
+    "Staff of earth" => 2126,
+    "Staff of fire" => 2574,
+    "Hard leather" => 8090,
+    "Huge plated mithril salvage" => 5961,
+    "Medium plated mithril salvage" => 2367,
+    "Small plated mithril salvage" => 932,
+    "Small bladed mithril salvage" => 3545,
+    "Medium bladed mithril salvage" => 4520,
+    "Mithril arrow" => 1300,
+    "Amulet of magic" => 1905,
+    "Amulet of magic (t)" => 1373998,
+    "Willow composite bow" => 1185291,
+    "Yew composite bow" => 1328367,
+    "Magic composite bow" => 1322405,
+    "Black crossbow" => 1309129,
+    "Offhand black crossbow" => 1503398,
+    "Black 2h crossbow" => 16368,
+    "Black bolts" => 4158
+)
+end
